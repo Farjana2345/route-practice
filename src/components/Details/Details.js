43970c7f01a67e89ba0a31ffeddded12cd9365d7
Farjana2345 <fa8547830@gmail.com>
2021-03-08 14:23:35 +0100

    first commit

---
+++ src/components/Details/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Details = (props) => {
+    
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Details;
---
+++ src/components/FriendDetail/FriendDetail.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+
+const FriendDetail = () => {
+    const{friendId}=useParams();
+    const[friend, setFriend] = useState({});
+    useEffect(()=>{
+        const url =`https://jsonplaceholder.typicode.com/users/${friendId}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=>setFriend(data))
+    },[]);
+    return (
+        <div>
+            <h3>this is friend detail {friendId} </h3>
+            <h4>Name:{friend.name}</h4>
+            <p>Email : {friend.email}</p>
+        </div>
+    );
+};
+
+export default FriendDetail;
